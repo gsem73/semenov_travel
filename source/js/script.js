1b2c11e9bf9139e94b7ade7f3000032f3c9f4e32
gsem73 <gh5t@yandex.ru>
2021-05-24 11:58:01 +0300

    JavaScript для блока header

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,22 @@
 'use strict';
+const header = document.querySelector('.header');
+const toggle = header.querySelector('.toggle');
+const menu = header.querySelector('.menu');
+
+toggle.classList.remove('toggle--cross');
+toggle.classList.add('toggle--line');
+
+menu.classList.remove('menu--show');
+menu.classList.add('menu--hide');
+
+const onToggleClick = function() {
+
+  toggle.classList.toggle('toggle--cross');
+  toggle.classList.toggle('toggle--line');
+
+  menu.classList.toggle('menu--show');
+  menu.classList.toggle('menu--hide');
+
+}
+
+toggle.addEventListener("click", onToggleClick);
